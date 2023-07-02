e2aab46a0969dd9ed1df37f363d1ff4364fa490f
monic vyas <118365854+Monic-19@users.noreply.github.com>
2023-07-03 02:41:31 +0530

    edit page 2

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
+//starting image 
+
+let simg = document.querySelector(".hero-right img");
+
+    gsap.from(simg , {
+        opacity: 0,
+        x:299,
+    })
+
+//about me section
+
+let a1= gsap.from(".about-left" ,{
+    opacity :0,
+    x:-600,
+    duration :1,
+    scrollTrigger:{
+        // markers:true,
+        trigger: ".about-left",
+        scroller : "body",
+        start : "25% 50%",
+        toggleActions: "restart none none reverse"
+    }
+})
+
+let a2= gsap.from(".about-right" ,{
+    opacity :0,
+    x:600,
+    duration :1,
+    scrollTrigger:{
+        // markers:true,
+        trigger: ".about-right",
+        scroller : "body",
+        start : "25% 50%",
+        toggleActions: "restart none none reverse"
+    }
+})
+
 // Skills Section
-var tl3 = gsap.timeline({
+var tl1 = gsap.timeline({
     scrollTrigger: {
         trigger: ".skills",
         start: "50% 50%",
@@ -11,7 +48,7 @@ var tl3 = gsap.timeline({
     }
 });
 
-tl3
+tl1
     .to(".top", {
         top: "-50%"
     }, 'a')
@@ -34,17 +71,31 @@ tl3
     })
 
     .to(".swiper", {
-        delay : .5,
+        delay : "2",
         zIndex: "1",
     },'a')
 
+
+    let tl2 = gsap.from(".content h2,.skill h3, .skill li", {
+        opacity: 0,
+        stagger: .31,
+        x: -100,
+        scrollTrigger: {
+            trigger: ".content",
+            scroller: 'body',
+            // markers: true,
+            start: "85% 50%",
+            toggleActions: "restart none none reverse"
+        }
+    })
+
+
     //cube
 
-
-var swiper = new Swiper(".mySwiper", {
+    var swiper = new Swiper(".mySwiper", {
     effect: "cube",
     grabCursor: true,
-
+    
     cubeEffect: {
         shadow: true,
         slideShadows: true,
@@ -60,22 +111,23 @@ var swiper = new Swiper(".mySwiper", {
     // pagination: {
     //     el: ".swiper-pagination",
     // },
-});
-
-    // let tl2 = gsap.from(".content h5,.content h2, .content h3", {
-    //     opacity: 0,
-    //     stagger: 1,
-    //     x: -100,
-    //     scrollTrigger: {
-    //         trigger: ".content",
-    //         scroller: 'body',
-    //         // markers: true,
-    //         start: "65% 50%",
-    //         toggleActions: "restart none none reverse"
-    //     }
-    // })
+    });
 
 
-    //about
 
- 
+//contact
+
+let c1= gsap.from(".contact , .footer-icons " ,{
+    opacity :0,
+    duration :2,
+    stagger:1,
+    scrollTrigger:{
+        // markers:true,
+        trigger: ".contact ,.footer-icons",
+        scroller : "body",
+        start : "15% 50%",
+        toggleActions: "restart none none reverse"
+    }
+})
+
+
